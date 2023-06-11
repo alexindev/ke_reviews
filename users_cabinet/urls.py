754ec31f9efea3fe478a5ca6fd0210b787c8c34f
alex <eruslanov-opt@yandex.ru>
2023-06-11 23:20:54 +0300

    add categories to menu

--- users_cabinet/urls.py
+++ users_cabinet/urls.py
@@ -6,7 +6,11 @@ from .views import *
 app_name = 'users_cabinet'
 
 urlpatterns = [
-    path('', login_required(ProfileUpdateView.as_view()), name='users_profile_url'),
-    path('logout/', logout, name='logout_url')
+    path('dashboard/', login_required(ProfileDetailView.as_view()), name='users_profile_url'),
+    path('settings/', login_required(SettingsTemplateView.as_view()), name='profile_settings_url'),
+    path('parser/', login_required(ParserTemplateView.as_view()), name='parser_url'),
+    path('reviews/', login_required(ReviewsTemplateView.as_view()), name='reviews_url'),
+    path('logout/', logout, name='logout_url'),
+
 
 ]
--- users_cabinet/views.py
+++ users_cabinet/views.py
@@ -1,12 +1,34 @@
 from common.title import TitleMixin
-from django.views.generic.base import TemplateView
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 from django.contrib import auth
 from django.shortcuts import reverse, HttpResponseRedirect
+from users.models import Users
 
 
-class ProfileUpdateView(TitleMixin, TemplateView):
-    template_name = 'users_cabinet/index.html'
-    title = 'Личный кабинет'
+class ProfileDetailView(TitleMixin, ListView):
+    template_name = 'users_cabinet/profile.html'
+    model = Users
+    title = 'Главное меню'
+
+
+class SettingsTemplateView(TitleMixin, ListView):
+    template_name = 'users_cabinet/settings.html'
+    model = Users
+    title = 'Настройки'
+
+
+class ParserTemplateView(TitleMixin, ListView):
+    template_name = 'users_cabinet/parser.html'
+    model = Users
+    title = 'Парсер'
+
+
+class ReviewsTemplateView(TitleMixin, ListView):
+    template_name = 'users_cabinet/reviews.html'
+    model = Users
+    title = 'Отзывы'
+
 
 def logout(request):
     auth.logout(request)
