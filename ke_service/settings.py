0f4f57446686556f43b7b8e24f70572795e56cef
alex <eruslanov-opt@yandex.ru>
2023-06-12 22:32:59 +0300

    del left files

--- ke_service/settings.py
+++ ke_service/settings.py
@@ -126,8 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+if DEBUG:
+    STATICFILES_DIRS = [BASE_DIR / 'static']
+else:
+    STATIC_ROOT = BASE_DIR / 'static'
 
 
 # Default primary key field type
