8123a9247f13631450003b5de8eb252c1d763871
alex <eruslanov-opt@yandex.ru>
2023-07-01 07:30:30 +0300

    task for get reviews

---
+++ users_cabinet/utils/reviews.py
@@ -0,0 +1,49 @@
+import datetime
+import requests
+
+
+def get_reviews(token: str) -> list:
+    headers = {
+        'accept': 'application/json',
+        'authorization': f'Bearer {token}',
+    }
+
+    reviews_list = []
+    count_list = []
+    page = 0
+    work = True
+
+    while work:
+        response = get_data(page, headers)
+
+        for data in response['payload']:
+            timestamp_create = data['dateCreated'] / 1000
+            review = {
+                'store': data['shop']['title'],
+                'product': data['product']['productTitle'],
+                'rating': data['rating'],
+                'review_id': data['reviewId'],
+                'content': data['content'],
+                'date_create': datetime.datetime.fromtimestamp(timestamp_create).strftime('%Y-%m-%d'),
+            }
+            reviews_list.append(review)
+            count_list.append(data)
+            page += 1
+
+            if len(count_list) % 100 != 0:
+                work = False
+    return reviews_list
+
+
+def get_data(page: int, headers: dict):
+    params = {
+        'page': f'{page}',
+        'filter': 'NO_REPLY',
+        'size': '100',
+    }
+
+    return requests.get(
+        'https://api.business.kazanexpress.ru/api/seller/product-reviews',
+        params=params,
+        headers=headers,
+    ).json()
