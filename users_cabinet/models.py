730a875621baaa238dd89080ea1ec6cc091ec710
alex <eruslanov-opt@yandex.ru>
2023-07-09 22:08:38 +0300

    add store_name, correct models

--- users_cabinet/models.py
+++ users_cabinet/models.py
@@ -4,13 +4,11 @@ from users.models import Users
 
 
 class ProductData(models.Model):
-    store_name = models.CharField(max_length=50, null=True)
     product = models.CharField(max_length=200, null=True)
     price = models.PositiveIntegerField(default=0)
-    sold = models.PositiveIntegerField(default=0)
     stock_balance = models.PositiveIntegerField(null=True)
     url = models.CharField(max_length=100)
-    rating = models.FloatField(null=True)
+    rating = models.FloatField(null=True, blank=True)
     param1 = models.CharField(max_length=50, null=True)
     param2 = models.CharField(max_length=50, null=True)
     datetime = models.DateField(auto_now_add=True)
@@ -22,19 +20,20 @@ class ProductData(models.Model):
         verbose_name_plural = 'Детализация по продажам'
 
     def __str__(self):
-        return str(self.store)
+        return str(self.product)
 
 class Stores(models.Model):
+    store_name = models.CharField(max_length=50, null=True, blank=True)
     store_url = models.CharField(max_length=100, null=True, blank=True)
     action = models.CharField(max_length=5, default='play')
-    users = models.ManyToManyField(Users, through='ProductData', related_name='stores')
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = 'магазин'
         verbose_name_plural = 'Все магазины'
 
     def __str__(self):
-        return self.store_url
+        return self.store_name
 
 
 class Reviews(models.Model):
