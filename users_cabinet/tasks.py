c413772b4cd06b88a7e1908afb90c541ded5250a
alex <eruslanov-opt@yandex.ru>
2023-07-09 22:17:27 +0300

    create task for parser

--- users_cabinet/tasks.py
+++ users_cabinet/tasks.py
@@ -1,8 +1,11 @@
 from celery import shared_task
-from .utils.token import get_token
-from .utils.reviews import get_reviews
+
 from users.models import Users
-from users_cabinet.models import Reviews
+from users_cabinet.models import *
+
+from users_cabinet.utils.token import get_token
+from users_cabinet.utils.reviews import get_reviews
+from users_cabinet.utils.parser import ProductId, ProductSKU
 
 
 @shared_task
@@ -37,6 +40,33 @@ def review_manager(token, user_pk) -> bool:
             )
         return True
     return False
+
+
+@shared_task
+def parser(store_name: str, user_pk: str):
+    url = 'https://kazanexpress.ru/' + store_name
+    product_ids = ProductId(url)
+    sku_data = ProductSKU()
+    user = Users.objects.get(pk=user_pk)
+    store = Stores.objects.get(store_url=url)
+    for product_id_set in product_ids.get_product_id():
+        for product_id in product_id_set:
+            for sku_tuple in sku_data.get_product_data(product_id):
+                product_name, price, available, rating, params = sku_tuple[:5]
+                param1 = params[0] if params else None
+                param2 = params[1] if len(params) > 1 else None
+
+                ProductData.objects.create(
+                    product=product_name,
+                    price=price,
+                    stock_balance=available,
+                    url=f'https://kazanexpress.ru/product/{product_id}',
+                    rating=rating if rating else None,
+                    param1=param1,
+                    param2=param2,
+                    user=user,
+                    store=store
+                )
 
 
 @shared_task
