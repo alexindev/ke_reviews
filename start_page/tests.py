9c50b339deebb53fd2e4fde64b581c2605b11200
alex <eruslanov-opt@yandex.ru>
2023-07-31 17:55:38 +0300

    create tests for start_page

--- start_page/tests.py
+++ start_page/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class StartPageTestCase(TestCase):
+
+    def test_load_start_page(self):
+        """Загрузка стартовой страницы"""
+        response = self.client.get(reverse('start_page:start_page_url'))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(response.context_data.get('title'), 'Главная страница')
+
+    def test_404_page(self):
+        """Страница 404"""
+        response = self.client.get('/page/that/does/not/exist/')
+        self.assertEqual(response.status_code, 404)
+        self.assertTemplateUsed(response, 'page404.html')
