bac743548660c92e552c8ca7c07db69794167795
alexindev <eruslanov-opt@yandex.ru>
2023-08-18 19:30:50 +0300

    change tests to api

--- users/tests.py
+++ users/tests.py
@@ -7,72 +7,65 @@ from users.models import User
 class UserAuthRegisterTestCace(TestCase):
     """Авторизация и регистрация пользователей"""
 
-    @classmethod
-    def setUpTestData(cls):
-        cls.auth_page = reverse('users:auth_page_url')
-        cls.reg_page = reverse('users:reg_page_url')
-
     def test_auth_page(self):
         """Страница авторизации"""
-        response = self.client.get(self.auth_page)
+        response = self.client.get(reverse('users:auth_page_url'))
+
         self.assertEqual(response.status_code, 200)
         self.assertIn(response.context_data.get('title'), 'Авторизация пользователей')
 
-    def test_auth_false(self):
-        """Неудачная авторизация"""
-        username = 'test_user'
-        password = '11111'
-        response = self.client.post(self.auth_page, data={
-            'username': username,
-            'password': password
-        })
+    def test_reg_page(self):
+        """Страница регистрации"""
+        response = self.client.get(reverse('users:reg_page_url'))
+
         self.assertEqual(response.status_code, 200)
-        self.assertTrue(response.context_data.get('form').error_messages)
+        self.assertIn(response.context_data.get('title'), 'Регистрация пользователей')
 
     def test_auth_true(self):
         """Успешная авторизация"""
         username = 'testuser'
         password = 'testPASS123'
         user = User.objects.create_user(username=username, password=password)
-        response = self.client.post(self.auth_page, data={
+        response = self.client.post(reverse('rest:auth'), data={
             'username': user.username,
             'password': password
         })
-        self.assertEqual(response.status_code, 302)
-        self.assertTrue(user.is_authenticated)
-        self.assertEqual(response.url, reverse('users_cabinet:users_profile_url'))
-
-    def test_reg_page(self):
-        """Страница регистрации"""
-        response = self.client.get(self.reg_page)
         self.assertEqual(response.status_code, 200)
-        self.assertIn(response.context_data.get('title'), 'Регистрация пользователей')
+        self.assertTrue(user.is_authenticated)
 
-    def test_reg_false(self):
-        """Неудачная регистрация"""
+    def test_auth_false(self):
+        """Неудачная авторизация. Пользователя не существует"""
         username = 'test_user'
         password = '11111'
-        response = self.client.post(self.reg_page, data={
+        response = self.client.post(reverse('rest:auth'), data={
             'username': username,
-            'password1': password,
-            'password2': password,
+            'password': password
         })
-
-        self.assertEqual(response.status_code, 200)
-        self.assertTrue(response.context_data.get('form').error_messages)
+        self.assertEqual(response.status_code, 401)
 
     def test_reg_true(self):
         """Успешная регистрация"""
         username = 'test_user'
         password = 'testPASS123'
-        response = self.client.post(self.reg_page, data={
+        response = self.client.post(reverse('rest:register'), data={
+            'username': username,
+            'password': password,
+        })
+        new_user = User.objects.get(username=username)
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(new_user.username, username)
+        self.assertTrue(new_user.is_authenticated)
+
+    def test_reg_false(self):
+        """Неудачная регистрация. Пользователь уже существует"""
+        username = 'test_user'
+        password = '11111'
+        User.objects.create_user(username=username, password=password)
+        response = self.client.post(reverse('rest:register'), data={
             'username': username,
             'password1': password,
             'password2': password,
         })
 
-        new_user = User.objects.get(username=username)
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response.url, reverse('users:auth_page_url'))
-        self.assertEqual(new_user.username, username)
-        self.assertTrue(new_user.is_authenticated)
+        self.assertEqual(response.status_code, 400)
