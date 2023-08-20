b1f3d4505ebf443d0899e61dcee4211827101ec0
alexindev <eruslanov-opt@yandex.ru>
2023-08-20 19:38:55 +0300

    add names

--- rest/urls.py
+++ rest/urls.py
@@ -4,15 +4,15 @@ from .views import *
 app_name = 'rest'
 
 urlpatterns = [
-    path('store_status/', UpdateStoreStatusView.as_view()),
-    path('new_store/', NewStoreView.as_view()),
-    path('review/', ReviewSettingsView.as_view()),
-    path('avatar/', UserPicView.as_view()),
-    path('delete_store/', DeleteStoreView.as_view()),
-    path('get_token/', GetNewTokenView.as_view()),
-    path('get_task_status/', GetTaskStatusView.as_view()),
-    path('get_reviews/', ReviewsShowView.as_view()),
-    path('update_reviews/', ReviewsUpdateView.as_view()),
+    path('store_status/', UpdateStoreStatusView.as_view(), name='store_status'),
+    path('new_store/', NewStoreView.as_view(), name='new_store'),
+    path('review/', ReviewSettingsView.as_view(), name='review'),
+    path('avatar/', UserPicView.as_view(), name='avatar'),
+    path('delete_store/', DeleteStoreView.as_view(), name='delete_store'),
+    path('get_token/', GetNewTokenView.as_view(), name='get_token'),
+    path('get_task_status/', GetTaskStatusView.as_view(), name='get_task_status'),
+    path('get_reviews/', ReviewsShowView.as_view(), name='get_reviews'),
+    path('update_reviews/', ReviewsUpdateView.as_view(), name='update_reviews'),
     path('auth/', UserAuthView.as_view(), name='auth'),
     path('register/', UserRegisterView.as_view(), name='register'),
 ]
