176673ca70fda5eae411e328fdd81fd2e8c6dacb
alexindev <eruslanov-opt@yandex.ru>
2023-08-21 22:49:21 +0300

    delete dashboard

--- users_cabinet/urls.py
+++ users_cabinet/urls.py
@@ -6,7 +6,6 @@ from .views import *
 app_name = 'users_cabinet'
 
 urlpatterns = [
-    path('dashboard/', login_required(ProfileView.as_view()), name='users_profile_url'),
     path('settings/', login_required(SettingsView.as_view()), name='profile_settings_url'),
     path('parser/', login_required(ParserView.as_view()), name='parser_url'),
     path('reviews/', login_required(ReviewsView.as_view()), name='reviews_url'),
